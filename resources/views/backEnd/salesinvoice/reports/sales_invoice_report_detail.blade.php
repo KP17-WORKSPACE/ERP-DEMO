725eb87a59290d5eab9e623d9540af9fd0bfc4a2
Kunal Pandharkar <kp.workspace17@gmail.com>
2026-05-15 22:13:29 +0530

    Add files via upload

--- resources/views/backEnd/salesinvoice/reports/sales_invoice_report_detail.blade.php
+++ resources/views/backEnd/salesinvoice/reports/sales_invoice_report_detail.blade.php
@@ -28,37 +28,7 @@
             <div class="" role="tabpanel" aria-labelledby="data-tab" id="data-details">
                 <div class="purchase-order-content-header">
                     <div class="purchase-order-content-header-left">
-                        <div class="dropdown report-type-dropdown">
-                            @php
-                                $menuReportGroup = $report_group ?? 'date_wise';
-                                $isSalesInvoiceRoute = request()->routeIs('sales.invoice.report.detail');
-                                $isSalesReturnRoute = request()->routeIs('sales.return.report.detail');
-                            @endphp
-                            <a class="text-dark report-type-trigger" href="javascript:void(0);" id="salesReportTypeMenu"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Sales Report Type <i class="icon-outline-alt-arrow-down ms-1"></i>
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="salesReportTypeMenu">
-                                <li class="dropend">
-                                    <a class="dropdown-item dropdown-toggle report-submenu-trigger text-dark" href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">Sales Report</a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="text-dark dropdown-item {{ $isSalesInvoiceRoute && $menuReportGroup === 'company_wise' ? 'active' : '' }}" href="{{ route('sales.invoice.report.detail', ['report_group' => 'company_wise']) }}">Company Wise</a></li>
-                                        <li><a class="text-dark dropdown-item {{ $isSalesInvoiceRoute && $menuReportGroup === 'date_wise' ? 'active' : '' }}" href="{{ route('sales.invoice.report.detail', ['report_group' => 'date_wise']) }}">Date Wise</a></li>
-                                        <li><a class="text-dark dropdown-item {{ $isSalesInvoiceRoute && $menuReportGroup === 'customer_wise' ? 'active' : '' }}" href="{{ route('sales.invoice.report.detail', ['report_group' => 'customer_wise']) }}">Customer Wise</a></li>
-                                        <li><a class="text-dark dropdown-item {{ $isSalesInvoiceRoute && $menuReportGroup === 'sales_person_wise' ? 'active' : '' }}" href="{{ route('sales.invoice.report.detail', ['report_group' => 'sales_person_wise']) }}">Sales Person Wise</a></li>
-                                    </ul>
-                                </li>
-                                <li class="dropend">
-                                    <a class="dropdown-item dropdown-toggle report-submenu-trigger text-dark" href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">Sales Return Report</a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="text-dark dropdown-item {{ $isSalesReturnRoute && $menuReportGroup === 'company_wise' ? 'active' : '' }}" href="{{ route('sales.return.report.detail', ['report_group' => 'company_wise']) }}">Company Wise</a></li>
-                                        <li><a class="text-dark dropdown-item {{ $isSalesReturnRoute && $menuReportGroup === 'date_wise' ? 'active' : '' }}" href="{{ route('sales.return.report.detail', ['report_group' => 'date_wise']) }}">Date Wise</a></li>
-                                        <li><a class="text-dark dropdown-item {{ $isSalesReturnRoute && $menuReportGroup === 'customer_wise' ? 'active' : '' }}" href="{{ route('sales.return.report.detail', ['report_group' => 'customer_wise']) }}">Customer Wise</a></li>
-                                        <li><a class="text-dark dropdown-item {{ $isSalesReturnRoute && $menuReportGroup === 'sales_person_wise' ? 'active' : '' }}" href="{{ route('sales.return.report.detail', ['report_group' => 'sales_person_wise']) }}">Sales Person Wise</a></li>
-                                    </ul>   
-                                </li>
-                            </ul>
-                        </div>
+                        @include('backEnd.partials.salesReportTypeMenu', ['menuReportGroup' => $report_group ?? 'date_wise'])
                     </div>
                     <div class="purchase-order-content-header-right">
                         <button type="button" class="btn btn-light" id="exportSalesInvoiceReport" title="Export to Excel">
